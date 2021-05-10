7f787427b255dabf938c85acc6757849e42e59db
Bangmond <dembbuto@naver.com>
2021-05-11 00:24:42 +0900

    Landing Page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/movie/';
-
+export const API_KEY = 'a6811cfdd3af058c24dad481336b1de9';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
