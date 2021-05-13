735328061f129269cacec734b0de6701ae7a7c02
Bangmond <dembbuto@naver.com>
2021-05-13 15:24:53 +0900

    Toggle Actor View

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from '../LandingPage/Sections/MainImage';
 import MovieInfo from './Sections/MovieInfo';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function MovieDetail(props) {
 	let movieId = props.match.params.movieId;
 	const [Movie, setMovie] = useState([]);
+	const [Casts, setCasts] = useState([]);
+	const [ActorToggle, setActorToggle] = useState(false);
+
 	useEffect(() => {
-		const endpointCrew = `${API_URL}${movieId}/credits?api_key=${API_KEY}`;
 		const endpointInfo = `${API_URL}${movieId}?api_key=${API_KEY}`;
 		fetch(endpointInfo)
 			.then(response => response.json())
@@ -15,7 +19,19 @@ function MovieDetail(props) {
 				console.log(response);
 				setMovie(response);
 			});
+
+		const endpointCast = `${API_URL}${movieId}/credits?api_key=${API_KEY}`;
+		fetch(endpointCast)
+			.then(response => response.json())
+			.then(response => {
+				console.log('responseForCast', response);
+				setCasts(response.cast);
+			});
 	}, []);
+
+	const toggleActorView = () => {
+		setActorToggle(!ActorToggle);
+	};
 
 	return (
 		<div>
@@ -36,6 +52,29 @@ function MovieDetail(props) {
 			</div>
 
 			{/* Actors Grid*/}
+			<div
+				style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}
+			>
+				<button onClick={toggleActorView}>Toggle Actor View</button>
+			</div>
+
+			{ActorToggle && (
+				<Row gutter={[16, 16]}>
+					{Casts &&
+						Casts.map((cast, index) => (
+							<React.Fragment key={index}>
+								<GridCards
+									image={
+										cast.profile_path
+											? `${IMAGE_BASE_URL}w500${cast.profile_path}`
+											: null
+									}
+									characterName={cast.name}
+								/>
+							</React.Fragment>
+						))}
+				</Row>
+			)}
 
 			{/* Comments */}
 		</div>
