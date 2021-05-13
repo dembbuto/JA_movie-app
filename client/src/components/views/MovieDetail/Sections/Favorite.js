f6ba19a289f16a6dbe3bd5c1b8b152855169265a
Bangmond <dembbuto@naver.com>
2021-05-13 17:10:05 +0900

    Favorite list add & remove

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+import { Button } from 'antd';
 
 function Favorite(props) {
-	const movieId = props.movieId;
 	const userFrom = props.userFrom;
+	const movieId = props.movieId;
 	const movieTitle = props.movieInfo.movieTitle;
 	const moviePost = props.movieInfo.backdrop_path;
 	const movieRunTime = props.movieInfo.runtime;
@@ -11,12 +12,15 @@ function Favorite(props) {
 	const [FavoriteNumber, setFavoriteNumber] = useState(0);
 	const [Favorited, setFavorited] = useState(false);
 
-	useEffect(() => {
-		let variables = {
-			userFrom,
-			movieId,
-		};
+	let variables = {
+		userFrom,
+		movieId,
+		movieTitle,
+		moviePost,
+		movieRunTime,
+	};
 
+	useEffect(() => {
 		Axios.post('/api/favorite/favoriteNumber', variables).then(response => {
 			console.log('favoriteNumber', response.data);
 			if (!response.data.success) {
@@ -36,11 +40,35 @@ function Favorite(props) {
 		});
 	}, []);
 
+	const onClickFavorite = () => {
+		if (Favorited) {
+			Axios.post('/api/favorite/removeFromFavorite', variables).then(
+				response => {
+					if (!response.data.success) {
+						alert('Favorite 리스트에서 지우는데 실패 했습니다.');
+					} else {
+						setFavoriteNumber(FavoriteNumber - 1);
+						setFavorited(!Favorited);
+					}
+				},
+			);
+		} else {
+			Axios.post('/api/favorite/addFromFavorite', variables).then(response => {
+				if (!response.data.success) {
+					alert('Favorite 리스트에 추가하는데 실패 했습니다.');
+				} else {
+					setFavoriteNumber(FavoriteNumber + 1);
+					setFavorited(!Favorited);
+				}
+			});
+		}
+	};
+
 	return (
 		<div>
-			<button>
+			<Button onClick={onClickFavorite}>
 				{Favorited ? 'Not Favorite' : 'Add to Favorite'} {FavoriteNumber}
-			</button>
+			</Button>
 		</div>
 	);
 }
